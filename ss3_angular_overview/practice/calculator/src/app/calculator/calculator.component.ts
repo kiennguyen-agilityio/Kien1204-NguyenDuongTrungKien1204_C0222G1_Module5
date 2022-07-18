8cff564ff6c9638f1e470b9a8a91310dc377a4b1
Kien1204ngkien1204@gmail.com.com <nguyendtrungkien1204@gmail.com.com>
2022-07-18 17:10:31 +0700

    first commit

---
+++ ss3_angular_overview/practice/calculator/src/app/calculator/calculator.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+  number1 = 0;
+  number2 = 0;
+  operator = '+';
+  result = 0;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  calculate() {
+    // tslint:disable-next-line:no-eval
+    this.result = eval(this.number1 + this.operator + this.number2);
+  }
+}
---
+++ ss3_angular_overview/practice/color-piker/src/app/color/color.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-color',
+  templateUrl: './color.component.html',
+  styleUrls: ['./color.component.css']
+})
+export class ColorComponent implements OnInit {
+  background = '#f93301';
+  constructor() {
+  }
+  changeColor(color) {
+    this.background = color;
+  }
+  ngOnInit(): void {
+  }
+}
