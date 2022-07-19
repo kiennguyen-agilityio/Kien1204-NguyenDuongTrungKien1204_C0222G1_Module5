6eaabfee47b34e9f40e99baa13d1e9a2bc113706
Kien1204ngkien1204@gmail.com.com <nguyendtrungkien1204@gmail.com.com>
2022-07-19 16:10:19 +0700

    first commit

--- case_study/angular/furama-angular/src/app/app.module.ts
+++ case_study/angular/furama-angular/src/app/app.module.ts
@@ -5,13 +5,29 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { FacilityComponent } from './facility/facility.component';
+import { ListFacilityComponent } from './list-facility/list-facility.component';
+import { UpdateFacilityComponent } from './update-facility/update-facility.component';
+import { AddNewServiceComponent } from './add-new-service/add-new-service.component';
+import { ListCustomerComponent } from './list-customer/list-customer.component';
+import { UpdateCustomerComponent } from './update-customer/update-customer.component';
+import { AddNewCustomerComponent } from './add-new-customer/add-new-customer.component';
+import { AddNewContractComponent } from './add-new-contract/add-new-contract.component';
+import { ListContractComponent } from './list-contract/list-contract.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    FacilityComponent
+    FacilityComponent,
+    ListFacilityComponent,
+    UpdateFacilityComponent,
+    AddNewServiceComponent,
+    ListCustomerComponent,
+    UpdateCustomerComponent,
+    AddNewCustomerComponent,
+    AddNewContractComponent,
+    ListContractComponent
   ],
   imports: [
     BrowserModule
